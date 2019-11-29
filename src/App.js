27c6c8f90827583a3f76623e52676d946a7c70b6
Changretta <johnylaf94@gmail.com>
2019-11-30 00:30:13 +0100

    final sprint day 1

--- src/App.js
+++ src/App.js
@@ -22,13 +22,19 @@ class App extends React.Component {
   
   
   render(){
-
+    const { monsters , searchField } = this.state;
+// const monsters = this.state.monsters;
+// const searchField = this.state.searchField;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
 
     return (
       <div className="App">
-      <input type='search' placeholder='search monsters' onChange={e =>  this.setState({searchField: e.target.value}, () => console.log(this.state) )
+      <input type='search' 
+             placeholder='search monsters' 
+             onChange={e =>  this.setState({searchField: e.target.value})
     } />
-      <CardList monsters={this.state.monsters}>
+      <CardList monsters={filteredMonsters}>
       </CardList>
       </div>
     );
